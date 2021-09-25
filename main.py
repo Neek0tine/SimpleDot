57ae9d9cc726822d471511072960eacc01e14708
Neekotine <77283651+Neek0tine@users.noreply.github.com>
2021-09-25 12:10:48 +0700

    Fixed cursor showing on top of window

--- main.py
+++ main.py
@@ -9,11 +9,16 @@ tooltip = 'SimpleDot'
 
 window = Window('Window that stays open',
                 layout,
+                grab_anywhere=False,
+                auto_size_buttons=False,
                 border_depth=0,
+                return_keyboard_events=False,
+                use_default_focus=False,
                 no_titlebar=True,
                 keep_on_top=True,
                 size=(8, 8),
-                resizable=False)
+                resizable=False,
+                finalize=True)
 tray = SystemTray(menu, single_click_events=False, window=window, tooltip=tooltip, icon='logo.ico')
 
 while True:
