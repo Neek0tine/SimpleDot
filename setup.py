b71564f5d10720f8612f416147b151f790a3c4bf
Neekotine <77283651+Neek0tine@users.noreply.github.com>
2021-09-28 02:05:51 +0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='AnimeKisa-Downloader',
+    version='0.2',
+    packages=[''],
+    url='https://github.com/Neek0tine/SimpleDot',
+    license='GPL v03',
+    author='Neekotine',
+    author_email='indefinick@gmail.com',
+    description='An overlayed center-screen dot (crosshair) for gaming'
+)
